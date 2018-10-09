ce9593ff3e4797a3776a46622ea8207e4c80cca2
Karen Birgisdóttir <karenbirgisdottir@KarenvalkyrjaMacBook-Pro.local>
2018-10-09 17:22:49 +0000

    verkefni1test

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var Svar1 = "Ert þú" + " ok?";
+
+var tala2 = 10+10*2/2-1;
+
+var svar3=true||false&&!true;
+
+
+//Hér er annað dæmi um svar 3
+/*var ÞaðErÞridjudagur = true;
+var ÞaðErFostudagur = false;
+var test1= ÞaðErÞridjudagur && ÞaðErFostudagur;
+var test2= ÞaðErÞridjudagur || ÞaðErÞridjudagur;
+var test3= !ÞaðErFostudagur;*/
+
+
+var svar4=(false>true)===(false<true);
+var svar5=(false<=true)!==(false>=true);
+
+var svar6 = "Kalli er góður strákur er jafnt og kalli er góður strákur" + " sem þýðir að Kalli er góður strákur TRUE";
+var svar7 = "Palli er vondur strákur er jafnt og (false og false)" + "sem þýðir false og false er FALSE";
+
+var svar8a = "false";
+var svar8b = "true";
+var svar8c = "false";
+var svar8d = "true";
+
+var svar9 = "true";
